f0a9fe102b95512da79c41747ce299f148a26527
손민석 <zonins3@gmail.com>
2026-02-26 21:47:38 +0900

    섹션3. Zustand 미들웨어 살펴보기 Part Two

--- onebite-exam/src/store/count.ts
+++ onebite-exam/src/store/count.ts
@@ -1,5 +1,11 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 // 스토어 타입 정의
@@ -13,37 +19,66 @@ import { immer } from "zustand/middleware/immer";
 
 // combine 미들웨어 활용
 export const useCountStore = create(
-  immer(
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          get();
-          // const count = get().count; // 현재 스토어의 count state 불러오기
-          // set({ count: count + 1 }); // 현재 스토어 업데이트, 명시되어 있는 프로퍼티 있으면 그 프로퍼티가 업데이트한다.
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                get();
+                // const count = get().count; // 현재 스토어의 count state 불러오기
+                // set({ count: count + 1 }); // 현재 스토어 업데이트, 명시되어 있는 프로퍼티 있으면 그 프로퍼티가 업데이트한다.
 
-          // 함수형 업데이트
-          // 함수형 업데이트로 많이 사용함
-          // set((state) => ({
-          //   count: state.count + 1,
-          // }));
+                // 함수형 업데이트
+                // 함수형 업데이트로 많이 사용함
+                // set((state) => ({
+                //   count: state.count + 1,
+                // }));
 
-          set((state) => {
-            state.count += 1;
-          });
-        }, // action
-        decrease: () => {
-          // 함수형 업데이트
-          // set((state) => ({
-          //   count: state.count - 1,
-          // }));
+                set((state) => {
+                  state.count += 1;
+                });
+              }, // action
+              decrease: () => {
+                // 함수형 업데이트
+                // set((state) => ({
+                //   count: state.count - 1,
+                // }));
 
-          set((state) => {
-            state.count -= 1;
-          });
-        }, // action
+                set((state) => {
+                  state.count -= 1;
+                });
+              }, // action
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
       },
-    })),
+    ),
+    {
+      name: "countStore",
+    },
   ),
+);
+
+// store의 count 상태를 구독, count의 값을 변경 할때 마다 count 출력
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // listner 함수
+    console.log(count, prevCount);
+
+    // 스토어 업데이트 해보기
+    const store = useCountStore.getState();
+    // useCountStore.setState((store) => ({ count: 10 }));
+  },
 );
 
 // 스토어 생성
