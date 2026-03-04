e99de4c8f32353e31f4d82c47a5169f186326460
손민석 <zonins3@gmail.com>
2026-03-05 08:51:51 +0900

    섹션 4. 캐시 데이터 다루기 2 - 응답 결과값 이용하기

--- onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { fetchCreateTodo } from "@/api/fetch-create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -9,9 +10,13 @@ export function useCreateTodoMutation() {
     mutationFn: fetchCreateTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      // queryClient.invalidateQueries({
+      //   queryKey: QUERY_KEYS.todo.list,
+      // });
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodo) => {
+        if (!prevTodo) return [newTodo];
+        return [...prevTodo, newTodo];
       });
     },
     onError: (error) => {
