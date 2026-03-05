b3ab80f3623758ac3219d43ae0291927c161c4ee
손민석 <zonins3@gmail.com>
2026-03-05 16:22:07 +0900

    섹션 4. 캐시 정규화하기 1

--- onebite-exam/src/pages/todo-detail-page.tsx
+++ onebite-exam/src/pages/todo-detail-page.tsx
@@ -5,7 +5,7 @@ export default function TodoDetailPage() {
   const params = useParams();
   const id = params.id;
 
-  const { data, isLoading, error } = useTodoDataById(id);
+  const { data, isLoading, error } = useTodoDataById(String(id));
 
   if (isLoading) return <div>로딩 중 입니다...</div>;
 
