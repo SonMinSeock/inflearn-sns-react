10200a98313d0a6705d853201109d661d945f223
손민석 <zonins3@gmail.com>
2026-03-05 09:52:16 +0900

    섹션 4. 캐시 데이터 다루기 4 - 낙관적 업데이트 예외 사항 및 대처 방법

--- onebite-exam/src/api/fetch-update-todo.ts
+++ onebite-exam/src/api/fetch-update-todo.ts
@@ -6,7 +6,7 @@ import type { Todo } from "@/types";
  * Partial 제내릭 -> todo 모든 프로퍼티들을 옵셔널로 해준다. id는 무조건 받도록 한다.
  */
 export async function fetchUpdateTodo(todo: Partial<Todo> & { id: string }) {
-  const response = await fetch(`${API_URL}/todos/${todo.id}`, {
+  const response = await fetch(`${API_URL}/todos1111/${todo.id}`, {
     method: "PATCH",
     body: JSON.stringify(todo),
   });
--- onebite-exam/src/hooks/mutations/use-update-todo-mutaion.ts
+++ onebite-exam/src/hooks/mutations/use-update-todo-mutaion.ts
@@ -8,7 +8,15 @@ export function useUpdateTodoMutation() {
 
   return useMutation({
     mutationFn: fetchUpdateTodo,
-    onMutate: (updatedTodo) => {
+    onMutate: async (updatedTodo) => {
+      // 만약에 업데이트 하기전에 todos 조회하고 있는 중이면 조회 요청들을 취소하라고 할수있다.
+      // 진행중인 요청이 나중에 overwrite 하는 걸 방지.
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
+
       // 낙관적 업데이트
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
         if (!prevTodos) return [];
@@ -18,6 +26,25 @@ export function useUpdateTodoMutation() {
             ? { ...prevTodo, ...updatedTodo }
             : prevTodo,
         );
+      });
+
+      return { prevTodos };
+    },
+    onError: (error, variable, context) => {
+      // context는 onMutate 반환 하고 있는 값이다.
+      // 원상 복구
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos,
+        );
+      }
+    },
+    onSettled: () => {
+      // 만약에 낙관적으로 업데이트 한 것이 서버에 저장한게 다른걸로 저장할 수 있다. 무결성 깨질수 있다.
+      // 무효화 하고 서버로 불러온 값으로 갱신
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
       });
     },
   });
