291ad09dfe11fc12806e1fd185102702030c6f89
손민석 <zonins3@gmail.com>
2026-03-18 11:32:38 +0900

    섹션 7. 이미지 업로드 구현하기 Part One

--- onebite-log/src/components/modal/PostEditorModal.tsx
+++ onebite-log/src/components/modal/PostEditorModal.tsx
@@ -1,16 +1,31 @@
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { Button } from "@/components/ui/button";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, XIcon } from "lucide-react";
 import { usePostEditorModal } from "@/store/postEditorModal";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import { useCreatePost } from "@/hooks/mutations/post/useCreatePost";
 import { toast } from "sonner";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+
+type Image = {
+  file: File;
+  previewUrl: string;
+};
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
+
   const [content, setContent] = useState("");
+  const [images, setImages] = useState<Image[]>([]);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
     onSuccess: () => {
       close();
@@ -32,6 +47,28 @@ export default function PostEditorModal() {
     createPost(content);
   };
 
+  const handleSelectImages = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files);
+
+      files.forEach((file) => {
+        setImages((prev) => [
+          ...prev,
+          { file, previewUrl: URL.createObjectURL(file) },
+        ]);
+      });
+    }
+
+    // 초기화
+    event.target.value = "";
+  };
+
+  const handleDeleteImage = (image: Image) => {
+    setImages((prevImages) =>
+      prevImages.filter((item) => item.previewUrl !== image.previewUrl),
+    );
+  };
+
   // 입력 내용에 따라 textarea 높이를 자동으로 늘리고 줄임
   useEffect(() => {
     if (textareaRef.current) {
@@ -46,6 +83,7 @@ export default function PostEditorModal() {
     // 모달 열렸을때 textarea 요소에 포커스 되도록 한다.
     textareaRef.current?.focus();
     setContent("");
+    setImages([]);
   }, [isOpen]);
 
   return (
@@ -60,10 +98,43 @@ export default function PostEditorModal() {
           ref={textareaRef}
           onChange={(event) => setContent(event.target.value)}
         />
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept="image/*"
+          multiple
+          className="hidden"
+          onChange={handleSelectImages}
+        />
+        {images.length > 0 && (
+          <Carousel>
+            <CarouselContent>
+              {images.map((image) => (
+                <CarouselItem key={image.previewUrl} className="basis-2/5">
+                  <div className="relative">
+                    <img
+                      src={image.previewUrl}
+                      className="h-full w-full rounded-sm object-cover"
+                    />
+                    <div
+                      className="absolute top-0 right-0 cursor-pointer rounded-full bg-black/30 p-1"
+                      onClick={() => handleDeleteImage(image)}
+                    >
+                      <XIcon className="h-4 w-4 text-white" />
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        )}
         <Button
           disabled={isCreatePostPending}
           variant={"outline"}
           className="cursor-pointer"
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
         >
           <ImageIcon />
           이미지 추가
