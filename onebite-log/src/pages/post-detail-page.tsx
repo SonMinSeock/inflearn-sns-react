48734181931e9678683621a9431690bf4892f103
손민석 <zonins3@gmail.com>
2026-04-13 08:49:16 +0900

    섹션 8. 댓글 UI 구현하기

--- onebite-log/src/pages/post-detail-page.tsx
+++ onebite-log/src/pages/post-detail-page.tsx
@@ -1,3 +1,19 @@
+import CommentEditor from "@/components/comment/CommentEditor";
+import CommentList from "@/components/comment/CommentList";
+import PostItem from "@/components/post/PostItem";
+import { Navigate, useParams } from "react-router";
+
 export default function PostDetailPage() {
-  return <div>PostDetailPage</div>;
+  const { postId } = useParams();
+
+  if (!postId) return <Navigate to={"/"} replace />;
+
+  return (
+    <div className="flex flex-col gap-5">
+      <PostItem type="DETAIL" postId={Number(postId)} />
+      <div className="text-xl font-bold">댓글</div>
+      <CommentEditor />
+      <CommentList />
+    </div>
+  );
 }
